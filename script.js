63383cd883a32c207048e75c9e9fad6d72b3192d
salman936-wq <salmansardar936@gmail.com>
2026-05-09 00:23:00 +0600

    Logic knowing

---
+++ script.js
@@ -0,0 +1,9 @@
+// Jodi Button a click kore tahole event start hobe
+
+// Lagbe 
+// 
+// If interview count > 0 === interview id(noJobs) display none
+// Else if (Append) the interview activeded <div> in interviewtab
+//
+// 
+
